ce3ac7d1d9cabff57af10fbb203e3ffaafcd8ff6
Idan Lobel <lobelid@post.bgu.ac.il>
2021-11-13 17:14:40 +0200

    change trainer and beginning of customer

---
+++ assigment1/cmake-build-debug/CMakeFiles/assigment1.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/idanl/OneDrive - post.bgu.ac.il/SPL/assigment1/Customer.cpp" "C:/Users/idanl/OneDrive - post.bgu.ac.il/SPL/assigment1/cmake-build-debug/CMakeFiles/assigment1.dir/Customer.cpp.obj"
+  "C:/Users/idanl/OneDrive - post.bgu.ac.il/SPL/assigment1/Studio.cpp" "C:/Users/idanl/OneDrive - post.bgu.ac.il/SPL/assigment1/cmake-build-debug/CMakeFiles/assigment1.dir/Studio.cpp.obj"
+  "C:/Users/idanl/OneDrive - post.bgu.ac.il/SPL/assigment1/Trainer.cpp" "C:/Users/idanl/OneDrive - post.bgu.ac.il/SPL/assigment1/cmake-build-debug/CMakeFiles/assigment1.dir/Trainer.cpp.obj"
+  "C:/Users/idanl/OneDrive - post.bgu.ac.il/SPL/assigment1/assigment1.cpp" "C:/Users/idanl/OneDrive - post.bgu.ac.il/SPL/assigment1/cmake-build-debug/CMakeFiles/assigment1.dir/assigment1.cpp.obj"
+  "C:/Users/idanl/OneDrive - post.bgu.ac.il/SPL/assigment1/main.cpp" "C:/Users/idanl/OneDrive - post.bgu.ac.il/SPL/assigment1/cmake-build-debug/CMakeFiles/assigment1.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ assigment1/cmake-build-debug/CMakeFiles/assigment1.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assigment1.dir/Customer.cpp.obj"
+  "CMakeFiles/assigment1.dir/Studio.cpp.obj"
+  "CMakeFiles/assigment1.dir/Trainer.cpp.obj"
+  "CMakeFiles/assigment1.dir/assigment1.cpp.obj"
+  "CMakeFiles/assigment1.dir/main.cpp.obj"
+  "assigment1.exe"
+  "assigment1.exe.manifest"
+  "assigment1.lib"
+  "assigment1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/assigment1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
